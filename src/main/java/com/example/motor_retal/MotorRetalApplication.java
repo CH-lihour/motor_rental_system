bf9085a582fd0cbdc03a5fca8b9469adb5f073e8
Hour <239297656+CH-lihour@users.noreply.github.com>
2026-03-02 14:59:59 +0700

    Initial commit - mootor-rental Spring Boot project

---
+++ src/main/java/com/example/motor_retal/MotorRetalApplication.java
@@ -0,0 +1,13 @@
+package com.example.motor_retal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorRetalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorRetalApplication.class, args);
+	}
+
+}
